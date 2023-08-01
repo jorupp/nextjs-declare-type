124df7675ae4cb70cb8e046504554562812cb650
Jonathan Rupp <jrupp@rightpoint.com>
2023-08-01 08:48:43 -0500

    add @brightcove/react-player-loader and basic type

---
+++ src/app/component.tsx
@@ -0,0 +1,5 @@
+'use client';
+
+import ReactPlayerLoader from '@brightcove/react-player-loader';
+
+export const Component = () => <ReactPlayerLoader accountId=''/>;
---
+++ types/brightcove.d.ts
@@ -0,0 +1,32 @@
+// started this based on https://github.com/brightcove/react-player-loader and https://github.com/brightcove/player-loader#parameters
+//   but it is incomplete
+declare module '@brightcove/react-player-loader' {
+    type CatalogSearch =  {
+        q: string;
+        limit?: number;
+    }
+    type CatalogSequence = {
+        type: 'search',
+        id: CatalogSearch;
+    } | {
+        type: 'playlist',
+        id: string;
+    } | {
+        type: 'video',
+        id: string;
+    };
+    // this is key - `import React from 'react'` at the top of the file won't work - see https://stackoverflow.com/a/51114250
+    const ReactPlayerLoader: import('react').ComponentType<{
+        accountId: string;
+        adConfigId?: string;
+        applicationId?: string;
+        catalogSearch?: string | CatalogSearch;
+        catalogSequence?: CatalogSequence | CatalogSequence[];
+        attrs?: object;
+        baseUrl?: string;
+        manualReloadFromPropChanges?: boolean;
+        onSuccess?: () => void;
+        onFailure?: () => void;
+    }>;
+    export default ReactPlayerLoader;
+}
